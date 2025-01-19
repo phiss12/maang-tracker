fe13a445bd0a1340a29ccd9f7c9430e88474b6a7
Paul Hissen <phissen@uci.edu>
2025-01-19 02:57:22 -0800

    refactor: reverted changes

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,14 +16,6 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
         {/* Favicon */}
         <link rel="icon" href="/favicon.ico" />
 
-        {/* Additional Favicons & Icons */}
-        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
-        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
-        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
-        <link rel="icon" type="image/png" sizes="192x192" href="/android-chrome-192x192.png" />
-        <link rel="icon" type="image/png" sizes="512x512" href="/android-chrome-512x512.png" />
-
-        
         {/* Title */}
         <title>{metadata.title}</title>
       </Head>
